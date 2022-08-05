ba6500b97926153465077911d77464ee69d3bb3a
Niki Hite <nikihite@comcast.net>
2022-08-05 12:28:23 -0700

    cities post test passing

--- __tests__/cities.test.js
+++ __tests__/cities.test.js
@@ -50,6 +50,20 @@ describe('backend-express-template routes', () => {
     });
   });
 
+  it('#POST /cities should create a new city', async () => {
+    const newCity = {
+      city: 'Niki',
+      country: 'United States',
+      population: '1',
+    };
+    const resp = await request(app).post('/cities').send(newCity);
+    expect(resp.status).toBe(200);
+    expect(resp.body).toEqual({
+      id: expect.any(String),
+      ...newCity,
+    });
+  });
+
   afterAll(() => {
     pool.end();
   });
